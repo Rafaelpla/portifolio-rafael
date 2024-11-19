ef32e0bd99f928ee35c463cbc03e96c3734ebaf3
Rafaelpla <rafasutildacosta@gmail.com>
2024-11-19 10:44:30 -0300

    Created project and Navbar

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import NavBar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
